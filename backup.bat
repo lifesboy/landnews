052a3bac44a8202b6170cb882795282bc4310468
lifesboy <49264651+lifesboy@users.noreply.github.com>
2021-04-18 17:53:49 +0700

    backup failsafe stoping data

--- backup.bat
+++ backup.bat
@@ -1,5 +1,6 @@
 set tag=%date:~10,4%-%date:~7,2%-%date:~4,2%T%time:~0,2%
-docker-compose stop
+docker-compose stop kib01 es01 es02 es03
+docker-compose stop dat01
 docker commit dat01 lifesboy/landnews-data:%tag%
 docker tag lifesboy/landnews-data:%tag% lifesboy/landnews-data:latest
 docker push lifesboy/landnews-data:%tag%
--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 tag=$(date '+%Y-%m-%dT%H')
-docker-compose stop
+docker-compose stop kib01 es01 es02 es03
+docker-compose stop dat01
 docker commit dat01 lifesboy/landnews-data:$tag
 docker tag lifesboy/landnews-data:$tag lifesboy/landnews-data:latest
 docker push lifesboy/landnews-data:$tag
