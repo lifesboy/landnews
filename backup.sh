0729aee90b28c8b57a4f1d97397dcea41560e8f9
lifesboy <49264651+lifesboy@users.noreply.github.com>
2021-04-18 14:38:06 +0700

    enable backup tag

--- backup.sh
+++ backup.sh
@@ -1,4 +1,7 @@
+tag=$(date '+%Y-%m-%dT%H')
 docker-compose stop
-docker commit dat01 lifesboy/landnews-data:latest
+docker commit dat01 lifesboy/landnews-data:$tag
+docker tag lifesboy/landnews-data:$tag lifesboy/landnews-data:latest
+docker push lifesboy/landnews-data:$tag
 docker push lifesboy/landnews-data:latest
 docker-compose restart
